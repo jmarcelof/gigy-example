e92e448be7a5eab56fa5153c685d3f4917fe4b26
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-07-08 16:15:43 -0300

    Removes doc type commentaries

--- src/main/java/com/poc/repository/UserRepository.java
+++ src/main/java/com/poc/repository/UserRepository.java
@@ -4,18 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.poc.model.User;
 
-/**
- * Access to the user data. JpaRepository grants us convenient access methods here.
- */
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	
-	/**
-	 * Find a user by username
-	 *
-	 * @param username the user's username
-	 * @return user which contains the user with the given username or null.
-	 */
 	User findOneByUsername(String username);
-
 }
